81a95aeab3d6479eadacff5c8523830bc2f4827f
skyhill2003 <skyhill2003@gmail.com>
2023-03-12 11:25:52 +0100

    Initial commit

---
+++ src/main/java/tk/skyhill2003/essentials/Essentials.java
@@ -0,0 +1,17 @@
+package tk.skyhill2003.essentials;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Essentials extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
